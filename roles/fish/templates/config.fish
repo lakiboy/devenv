8b6b27e68dac0fce8a4b799001e824ca76722903
Dmitrijs Lakacauskis <d@dmitrijs.io>
2023-02-18 17:41:17 +0200

    Install postgres.

--- roles/fish/templates/config.fish
+++ roles/fish/templates/config.fish
@@ -1,1 +1,26 @@
 set -g fish_user_paths "{{ homebrew_prefix }}/bin" "{{ homebrew_prefix }}/sbin" $fish_user_paths
+
+#
+# Aliases
+#
+
+# Shortcuts
+alias p="cd ~/Projects"
+alias g="git"
+alias h="history"
+
+# List
+alias ll="ls -lhF"
+alias la="ls -lhaF"
+
+# Docker
+alias d="docker"
+alias dc="docker-compose"
+
+#
+# Exports
+#
+
+{% for item in fish_exports | dict2items %}
+set -x {{ item.key }} {{ item.value }}
+{% endfor %}
